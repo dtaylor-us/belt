8745edd4787a9dda98125bfdafa8e622a4eb8ebb
Derek <derektaylor.us@gmail.com>
2019-07-06 19:15:17 -0500

    final material design

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,32 @@
+const path = require('path');
+
+function tryResolve_(url, sourceFilename) {
+    // Put require.resolve in a try/catch to avoid node-sass failing with cryptic libsass errors
+    // when the importer throws
+    try {
+        return require.resolve(url, {paths: [path.dirname(sourceFilename)]});
+    } catch (e) {
+        return '';
+    }
+}
+
+function tryResolveScss(url, sourceFilename) {
+    // Support omission of .scss and leading _
+    const normalizedUrl = url.endsWith('.scss') ? url : `${url}.scss`;
+    return tryResolve_(normalizedUrl, sourceFilename) ||
+        tryResolve_(path.join(path.dirname(normalizedUrl), `_${path.basename(normalizedUrl)}`),
+            sourceFilename);
+}
+
+function materialImporter(url, prev) {
+    if (url.startsWith('@material')) {
+        const resolved = tryResolveScss(url, prev);
+        return {file: resolved || url};
+    }
+    return {file: url};
+}
+
+
 const autoprefixer = require('autoprefixer');
 
 module.exports = {
@@ -29,8 +58,8 @@ module.exports = {
                     {
                         loader: 'sass-loader',
                         options: {
-                            includePaths: ['./node_modules']
-                        }
+                            importer: materialImporter
+                        },
                     },
                 ]
             }

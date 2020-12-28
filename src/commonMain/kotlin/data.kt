2949d37605e3dbf60fa2d96c469e397d737f136d
dtaylor-us <derektaylor.us@gmail.com>
2020-12-28 02:27:48 -0600

    add navbar wrapper

--- src/commonMain/kotlin/data.kt
+++ src/commonMain/kotlin/data.kt
@@ -6,7 +6,7 @@ val beltList = mutableListOf(
     Belt(Rank.SIXTH.rank, Rank.SIXTH.order, Level.INTERMEDIATE.level, "NA", BeltColor.PURPLE.color),
     Belt(Rank.FIFTH.rank, Rank.FIFTH.order, Level.INTERMEDIATE.level, "NA", BeltColor.BLUE.color),
     Belt(Rank.FOURTH.rank, Rank.FOURTH.order, Level.INTERMEDIATE.level, "NA", BeltColor.BLUE_LEADER.color),
-    Belt(Rank.THIRD.rank, Rank.THIRD.order, Level.INTERMEDIATE.level, "NA", BeltColor.BROWN.color),
+    Belt(Rank.THIRD.rank, Rank.THIRD.order, Level.ADVANCED.level, "NA", BeltColor.BROWN.color),
     Belt(Rank.SECOND.rank, Rank.SECOND.order, Level.ADVANCED.level, "NA", BeltColor.RED.color),
     Belt(Rank.FIRST.rank, Rank.FIRST.order, Level.ADVANCED.level, "NA", BeltColor.RED_LEADER.color),
     Belt(Rank.NONE.rank, 11, "BLACK BELT", "NA", BeltColor.DPTY_BLACK.color),
